b24fa8c229a72941cb46e1c3d50216dc1a9737fc
cg666-debug <66543665+cg666-debug@users.noreply.github.com>
2020-10-30 23:36:19 +0800

    Add files via upload

---
+++ 2018202131/src/face_recognition/face_recognition/main.py
@@ -0,0 +1,28 @@
+from utils import *
+ 
+w,h = 360,240
+pid = [0.4,0.4,0]
+pError = 0
+startCounter = 0  # for no Flight 1   - for flight 0
+ 
+ 
+myDrone = initializeTello()
+ 
+while True:
+ 
+    ## Flight
+    if startCounter == 0:
+        myDrone.takeoff()
+        startCounter = 1
+ 
+    ## Step 1
+    img = telloGetFrame(myDrone,w,h)
+    ## Step 2
+    img, info = findFace(img)
+    ## Step 3
+    pError = trackFace(myDrone,info,w,pid,pError)
+    #print(info[0][0])
+    cv2.imshow('Image',img)
+    if cv2.waitKey(1) and 0xFF == ord('q'):
+        myDrone.land()
+        break
